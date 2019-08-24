aa2125479ef3c0d97a94a2791a4ca52d66b1a18a
adarsh <adarshapakala@gmail.com>
2019-08-24 21:32:53 +0530

    update main.py UI coding started

--- Main.py
+++ Main.py
@@ -2,15 +2,24 @@
 from gtts import gTTS
 import pygame
 import time
+from tkinter import *
+
+mainWindow = Tk()
+mainWindow.title("Abacus teaher")
+mainWindow.geometry('500x500')
+lbl1 = Label(mainWindow, text="Hello....Lets start...")
+lbl1.grid(column=4, row=1)
+
 pygame.mixer.init()
-tts = gTTS(text='Good morning', lang='en')
+tts = gTTS(text='10', lang='en')
 print("Save started ", time.ctime())
 tts.save('good.mp3')
 print("Save ended ", time.ctime())
 
 pygame.mixer.music.load("good.mp3")
 pygame.mixer.music.play()
-time.sleep(5)
+
+mainWindow.mainloop()
 
 
 
